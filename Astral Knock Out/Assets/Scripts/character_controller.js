343d3dfcb5b02db7f0df30f585e861df249b0e42
Thund3rDev <samuelriosdev@gmail.com>
2020-10-08 23:41:49 +0200

    Controller velocity on jump

--- Astral Knock Out/Assets/Scripts/character_controller.js
+++ Astral Knock Out/Assets/Scripts/character_controller.js
@@ -1,5 +1,5 @@
 class Character_Controller extends Phaser.GameObjects.Rectangle /*Sprite*/ {
-    constructor(scene, id, x, y, w, h, color, cursors, moveSpeed, jumpForce, actualHP, maxHP){
+    constructor(scene, id, x, y, w, h, color, cursors, moveSpeed, jumpForce, actualHP, maxHP) {
         super(scene, x, y, w, h, color);
 
         var that = this;
@@ -20,6 +20,7 @@ class Character_Controller extends Phaser.GameObjects.Rectangle /*Sprite*/ {
         scene.physics.world.enable(this);
         this.body.maxVelocity.x = moveSpeed;
         this.body.setCollideWorldBounds(true);
+        this.isOnAir = false;
 
         // Establecemos el evento update
         this.scene.events.on("update", this.update, this);
@@ -28,74 +29,98 @@ class Character_Controller extends Phaser.GameObjects.Rectangle /*Sprite*/ {
         //this.sprite = this.scene.add.sprite(x, y, 'character_' + id); COMENTAR;
         this.body.drag.x = 3500;
         console.log(this.body);
-        this.cursors.jump.on('down', function(event){
+        this.cursors.jump.on('down', function (event) {
             that.jump();
         });
-        this.cursors.left.on('down', function(event){
+        this.cursors.left.on('down', function (event) {
             that.moveLeft();
         });
-        this.cursors.left.on('up', function(event){
+        this.cursors.left.on('up', function (event) {
             that.stopLeft();
         });
-        this.cursors.right.on('down', function(event){
+        this.cursors.right.on('down', function (event) {
             that.moveRight();
         });
-        this.cursors.right.on('up', function(event){
+        this.cursors.right.on('up', function (event) {
             that.stopRight();
         });
     }// Fin constructor
 
-    die()
-    {
+    die() {
         console.log("ME MUEROOOOOO");
     }
 
-    update(time, delta){
+    update(time, delta) {
         // Físicas de personaje
-        if(this.body.onFloor()){
+        if (this.body.onFloor()) {
             this.numJumps = 1;
             this.body.drag.x = 3500;
+
+            // Anadido para corregir velocidad suelo tras aire
+            if (this.isOnAir) {
+                this.isOnAir = false;
+                if (this.body.velocity.x < -(0.1)) {
+                    this.body.velocity.x = -(this.moveSpeed);
+                    this.body.acceleration.x = -(1);
+                }
+                else if (this.body.velocity.x > 0.1) {
+                    this.body.velocity.x = (this.moveSpeed);
+                    this.body.acceleration.x = (1);
+                }
+                else {
+                    this.body.velocity.x = 0;
+                    this.body.acceleration.x = 0;
+                }
+            }
+
         }
     }// Fin update
 
-    jump(){
+    jump() {
         //console.log(this.id+": Salto");
-        if (this.body.onFloor() || this.numJumps == 1){
+        if (this.body.onFloor() || this.numJumps == 1) {
+            this.isOnAir = true;
             this.body.velocity.y = -this.jumpForce;
             this.numJumps--;
+
+            if (this.body.velocity.x < -(0.1))
+                this.body.velocity.x = -(this.moveSpeed / 2);
+            else if (this.body.velocity.x > 0.1)
+                this.body.velocity.x = (this.moveSpeed / 2);
+            else
+                this.body.velocity.x = 0;
         }
     }
 
-    moveLeft(){
-        if (this.body.onFloor()){
+    moveLeft() {
+        if (this.body.onFloor()) {
             this.body.velocity.x = -(this.moveSpeed);
-            this.body.acceleration.x = -(1);
-        }else {
+        } else {
             this.body.velocity.x = -(this.moveSpeed / 2);
-            this.body.acceleration.x = -(1);
         }
+        this.body.acceleration.x = -(1);
     }
-    stopLeft(){
-        if (this.body.velocity.x <= 0){
+    stopLeft() {
+        if (this.body.velocity.x <= 0) {
             this.body.acceleration.x = 0;
-            if (!this.body.onFloor()){
+            if (!this.body.onFloor()) {
                 this.body.drag.x = 500;
             }
         }
     }
-    moveRight(){
-        if (this.body.onFloor()){
+    moveRight() {
+        if (this.body.onFloor()) {
             this.body.velocity.x = (this.moveSpeed);
-            this.body.acceleration.x = (1);
-        }else {
+
+        } else {
             this.body.velocity.x = (this.moveSpeed / 2);
-            this.body.acceleration.x = (1);
         }
+        this.body.acceleration.x = (1);
     }
-    stopRight(){
-        if (this.body.velocity.x >= 0){
+    stopRight() {
+        if (this.body.velocity.x >= 0) {
             this.body.acceleration.x = 0;
-            if (!this.body.onFloor()){
+            if (!this.body.onFloor()) {
                 this.body.drag.x = 500;
             }
         }
