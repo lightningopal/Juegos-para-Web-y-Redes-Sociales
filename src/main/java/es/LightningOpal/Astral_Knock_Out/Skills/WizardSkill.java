52324d7ebb7e74d4e011445ea265eb5f05de2e7c
Mario <mario.belen99@gmail.com>
2020-11-17 23:52:13 +0100

    Wizard skill update

--- src/main/java/es/LightningOpal/Astral_Knock_Out/Skills/WizardSkill.java
+++ src/main/java/es/LightningOpal/Astral_Knock_Out/Skills/WizardSkill.java
@@ -10,6 +10,9 @@ public class WizardSkill extends Skill{
     private double damage;
     private int id;
 
+    private long elapsedTime;
+    private long startTime;
+
     public WizardSkill(Player caster, Player target, long duration, boolean collidePlaforms, double speed, double damage, int id){
         super(caster, target, duration, 25, 6.50, collidePlaforms);
         this.setMoveSpeed(speed);
@@ -44,7 +47,7 @@ public class WizardSkill extends Skill{
         }
 
         switch(this.id){
-            case 1: // Ángulo hacia arriba
+            case 1:
             if (this.IsFlipped()){
                 this.setVelY(this.getMoveSpeed() * 0.3);
             }else{
@@ -52,7 +55,7 @@ public class WizardSkill extends Skill{
             }
             this.facingAngle = -25;
             break;
-            case 2: // Ángulo hacia abajo
+            case 2:
             if (this.IsFlipped()){
                 this.setVelY(this.getMoveSpeed() * -0.3);
             }else{
@@ -74,18 +77,23 @@ public class WizardSkill extends Skill{
                 disable();
             }
         }, duration);
+        this.startTime = System.currentTimeMillis();
     }
 
     @Override
     public void disable() {
         super.disable();
         this.isActive = false;
+        this.elapsedTime = 0;
+        this.startTime = 0;
     }
 
     @Override
     public double impact() {
         super.impact();
         this.isActive = false;
+        this.elapsedTime = 0;
+        this.startTime = 0;
         // Causa daño al enemigo
         return target.damage(this.damage);
     }
@@ -94,12 +102,39 @@ public class WizardSkill extends Skill{
     public double impact(double hp) {
         super.impact();
         this.isActive = false;
+        this.elapsedTime = 0;
+        this.startTime = 0;
         // Causar daño
         return hp - this.damage;
     }
 
     @Override
     public void calculatePhysics(){
+        elapsedTime = System.currentTimeMillis() - startTime;
+        double remainingTime = duration - elapsedTime;
+        remainingTime = remainingTime / duration; // Va de 1 a 0
+        if (this.id == 1){
+            if (this.IsFlipped()){
+                this.setVelY(lerp(this.getMoveSpeed() * -0.7, this.getMoveSpeed() * 0.7, remainingTime));
+                this.facingAngle = lerp(25, -25, remainingTime);
+            }else{
+                this.setVelY(lerp(this.getMoveSpeed() * 0.7, this.getMoveSpeed() * -0.7, remainingTime));
+                this.facingAngle = lerp(25, -25, remainingTime);
+            }
+        }else if (this.id == 2){
+            if (this.IsFlipped()){
+                this.setVelY(lerp(this.getMoveSpeed() * 0.7, this.getMoveSpeed() * -0.7, remainingTime));
+                this.facingAngle = lerp(-25, 25, remainingTime);
+            }else{
+                this.setVelY(lerp(this.getMoveSpeed() * -0.7, this.getMoveSpeed() * 0.7, remainingTime));
+                this.facingAngle = lerp(-25, 25, remainingTime);
+            }
+        }
         this.applyVelocity2Position();
+    }
+
+    public double lerp(double a, double b, double f)
+    {
+        return a + f * (b - a);
     }
 }
